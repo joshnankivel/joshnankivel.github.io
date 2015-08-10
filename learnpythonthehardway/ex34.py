81999836cf531a213d61dd107982b2463721855d
Josh Nankivel <jnankivel@apple.com>
2015-08-10 09:01:44 -0700

    lesson 34

---
+++ learnpythonthehardway/ex34.py
@@ -0,0 +1,8 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The first (1st) animal is at 0 and is a %s." % animals[0]
+print "The second (2nd) animal is at 1 and is a %s." % animals[1]
+print "The third (3rd) animal is at 2 and is a %s." % animals[2]
+print "The fourth (4th) animal is at 3 and is a %s." % animals[3]
+print "The fith (5th) animal is at 4 and is a %s." % animals[4]
+print "The sixth (6th) animal is at 5 and is a %s." % animals[5]
